3cb1d2c2447e2d36250346d1386b3453f92c40c7
白岳 <445798420@qq.com>
2019-05-13 17:59:08 +0800

    CHART-3924
    修正了部分BUG

--- src/main/java/com/fr/canvas/CanvasAdapter.java
+++ src/main/java/com/fr/canvas/CanvasAdapter.java
@@ -20,7 +20,7 @@ public class CanvasAdapter {
     }
 
     public CanvasAdapter(int width, int height) {
-        this.canvas = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        this.canvas = new BufferedImage(width * RESOLUTION, height * RESOLUTION, BufferedImage.TYPE_INT_ARGB);
     }
 
     public BufferedImage getCanvas() {
@@ -28,11 +28,11 @@ public class CanvasAdapter {
     }
 
     public int getWidth() {
-        return canvas.getWidth()/ RESOLUTION;
+        return canvas.getWidth() / RESOLUTION;
     }
 
     public int getHeight() {
-        return canvas.getHeight()/ RESOLUTION;
+        return canvas.getHeight() / RESOLUTION;
     }
 
     public void setAttribute(String attribute, int value) {
@@ -69,7 +69,7 @@ public class CanvasAdapter {
         context.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         //默认的线条样式改变
         context.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f));
-        context.scale(RESOLUTION,RESOLUTION);
+        context.scale(RESOLUTION, RESOLUTION);
         if (contextAdapter == null) {
             this.contextAdapter = new ContextAdapter(context, canvas);
         } else {
